65dd2b8c4bf2e9fc22fe08c199bae1f70138684b
YuroslavPM <yuroslavpm11@gmail.com>
2026-03-30 13:15:18 +0300

    Add default color to getRandomColorByString() if undefined

--- src/utils/helpers/randomColor.ts
+++ src/utils/helpers/randomColor.ts
@@ -1,5 +1,6 @@
 export const getRandomColorByString = (input: string) => {
   let hash = 0;
+  if(!input) return "hsl(0, 70%, 55%)";
   for (let i = 0; i < input.length; i++) {
     hash = input.charCodeAt(i) + ((hash << 5) - hash);
   }
